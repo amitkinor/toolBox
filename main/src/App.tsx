b0bdd5c284e09811e8d5cb876509c10186b55bb4
Amit Kinor <amit.kinor@gmail.com>
2021-03-06 23:14:37 +0200

    restructure

---
+++ main/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UseReducerExample from './comps/UseReducerExample'
+
+function App() {
+  return (
+    <div className="App">
+      <UseReducerExample/>
+    </div>
+  );
+}
+
+export default App;
