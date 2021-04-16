46bd1970a528653021003859644072515111b094
Amit Kinor <amit.kinor@gmail.com>
2021-04-16 12:41:38 +0300

    removed node files

--- nodejs/forking+clusters/gotoSleep.ts
+++
@@ -1,9 +0,0 @@
-const sleep = (ms) => {
-	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
-};
-
-process.on('message',()=>{
-  console.log('Going to sleep')
-  sleep(2000);
-  process.send('good morning');
-})
